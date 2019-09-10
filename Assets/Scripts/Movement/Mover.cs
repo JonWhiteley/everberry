c6d8a22a3adc7d2fbb1d99128084911670bf40af
Jonathan Whiteley <>
2019-09-10 12:49:32 -0400

    Added ActionPriority and Ruins placed in Woodland

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -3,21 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using RPG.Combat;
 
 namespace RPG.Movement
 {
     public class Mover : MonoBehaviour
     {
         [SerializeField] Transform target;
+        NavMeshAgent navMeshAgent;
+
+        private void Start() {
+            {
+                navMeshAgent = GetComponent<NavMeshAgent>();
+            }
+        }
 
         void Update()
         {
             UpdateAnimator();
         }
 
+        public void StartMoveAction (Vector3 destination)
+        {
+            GetComponent<Fighter>().Cancel();
+            MoveTo(destination);
+        }
+
         public void MoveTo(Vector3 destination)
         {
             GetComponent<NavMeshAgent>().destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Stop() {
+            {
+                navMeshAgent.isStopped = true;
+            }
         }
 
         private void UpdateAnimator()
