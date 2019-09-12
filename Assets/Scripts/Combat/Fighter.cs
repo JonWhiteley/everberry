f42f2185220a6fd59fec1c1a102c130676a331e5
Jonathan Whiteley <>
2019-09-12 09:55:39 -0400

    Added combat.

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -6,10 +6,14 @@ namespace RPG.Combat
 {
     public class Fighter : MonoBehaviour, IAction
     {
+        [SerializeField] float timeBetweenAttacks = 1f;
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float weaponDamage = 5f;
         Transform target;
+        float timeSinceLastAttack = 0;
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
             if (target == null) return;
             if (!GetIsInRange())
             {
@@ -18,6 +22,17 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
+            }
+        }
+
+        private void AttackBehaviour()
+        {
+            if (timeSinceLastAttack > timeBetweenAttacks)
+            {
+                //This will trigger the Hit event
+            GetComponent<Animator>().SetTrigger("attack");
+            timeSinceLastAttack = 0;
             }
         }
 
@@ -30,6 +45,12 @@ namespace RPG.Combat
         {
             GetComponent<ActionScheduler>().StartAction(this);
             target = combatTarget.transform;
+        }
+        //This is an animation event
+        void hit()
+        {
+            Health healthComponent = target.GetComponent<Health>();
+            healthComponent.TakeDamage(weaponDamage);
         }
         public void Cancel()
         {
